39dc155abc6fb0483fa37d1a39a6d45ff254a59e
Mike G <keep.it.gully@gmail.com>
2012-11-04 03:33:43 -0500

    Added subreddit.go - Can retrieve info from subreddit listing (i.e.: /r/starcraft.json or /r/starcraft/new.json)
    Added reddit.go
    - Example client that takes #keyword from irc chat and lists the top 25 submissions from /r/keyword in the channel

---
+++ reddit.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"os"
+	"time"
+	"strings"
+	"log"
+)
+
+import (
+	"geddit"
+	irc "github.com/thoj/Go-IRC-Client-Library"
+)
+
+func slowSendIRC(s string, icon irc.IRCConnection) {
+	second := time.Second
+	//	index := len(s)
+	time.Sleep(second)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s subreddit", os.Args[0])
+		os.Exit(1)
+	}
+	// setup HTTP client
+	url := "http://reddit.com/r/" + os.Args[1] + ".json"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "/u/wtf_is_up ~ playing with Go-lang")
+	
+	// make request	
+	resp, err := client.Do(req)	
+	geddit.CheckError(err)
+
+	// read response
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	//fmt.Printf("%s\n", string(body))
+	geddit.CheckError(err)
+	
+	// parse JSON
+	var jRep geddit.Top
+	err = json.Unmarshal(body, &jRep)
+	geddit.CheckError(err)
+	fmt.Println(jRep)
+	icon := irc.IRC("first_world_problems", "hahawtflol")
+	err = icon.Connect("irc.rizon.net:6667")
+	geddit.CheckError(err)
+	icon.AddCallback("001", func(e *irc.IRCEvent) { icon.Join("#ahnenerbe") })
+	icon.AddCallback("JOIN", func(e *irc.IRCEvent) { 
+		//for _, str := range jRep.List() {		
+			//icon.SendRaw("PRIVMSG #geddit :" + str)
+		//	time.Sleep(time.Second*1) 
+		//}	
+	})
+	icon.AddCallback("PRIVMSG", func(e *irc.IRCEvent) {
+		if strings.HasPrefix(e.Message, "#") {
+			req, err := http.NewRequest("GET", "http://reddit.com/r/" + e.Message[1:] + ".json", nil)
+			req.Header.Set("User-Agent", "wtf_is_up ~ playing with Go-lang")
+	
+			// make request	
+			resp, err := client.Do(req)	
+			geddit.CheckError(err)
+			if resp.StatusCode != http.StatusOK {
+				log.Println("bad HTTP response: " + resp.Status)
+				return
+			}
+	
+			// read response
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			//fmt.Printf("%s\n", string(body))
+			geddit.CheckError(err)
+	
+			if strings.HasPrefix(string(body), "<") {
+				log.Println("returned xml; bad")
+				icon.SendRaw("PRIVMSG " + e.Arguments[0] + " :ERROR // bad subreddit // q-(v_v)z")
+				return
+			}
+			// parse JSON
+			var jRep geddit.Top
+			err = json.Unmarshal(body, &jRep)
+			geddit.CheckError(err)
+			for _, str := range jRep.List() {
+				icon.SendRaw("PRIVMSG " + e.Arguments[0] + " :" + str)
+				time.Sleep(time.Second*1)
+			}		
+		}
+		
+	})
+	icon.Loop()
+}
---
+++ subreddit.go
@@ -0,0 +1,118 @@
+package geddit 
+
+import (
+	"log"
+	"encoding/json"
+	"strings"
+)
+
+// nullable string -- to handle json returning null. 
+// if encoding/json expects a string, the api returning a null will cause problems 
+// (same for bool/int)
+type NullString string
+
+func (n *NullString) UnmarshalJSON(b []byte) (err error) {
+	if string(b) == "null" {
+		return nil
+	}
+	return json.Unmarshal(b, (*string)(n))
+}
+
+// nullable bool
+type NullBool bool
+
+func (n *NullBool) UnmarshalJSON(b []byte) (err error) {
+	if string(b) == "null" {
+		return nil
+	}
+	return json.Unmarshal(b, (*bool)(n))
+}
+
+// nullable float x_x
+type NullFloat float64
+
+func (n *NullFloat) UnmarshalJSON(b []byte) (err error) {
+	if string(b) == "null" {
+		return nil
+	}
+	return json.Unmarshal(b, (*float64)(n))
+}
+
+type EData struct {
+	Domain 				NullString 		`json:"domain"`
+	BannedBy 			NullString		`json:"banned_by"`
+	//MediaEmbed 			[]string	`json:"media_embed"`
+	Subreddit 			NullString		`json:"subreddit"`
+	SelftextHTML 		NullString		`json:"selftext_html"`
+	Selftext 			NullString		`json:"selftext"`
+	//Likes 				float64		`json:"likes"`
+	LinkFlairText 		NullString		`json:"link_flair_text"`
+	Id 					NullString		`json:"id"`
+	//Clicked 			bool			`json:"clicked"`
+	Title 				NullString		`json:"title"`
+	NumComments 		NullFloat		`json:"num_comments"`
+	Score 				NullFloat		`json:"score"`
+	ApprovedBy 			NullString		`json:"approved_by"`
+	//Over18 				bool		`json:"over_18"`
+	//Hidden 				bool		`json:"hidden"`
+	Thumbnail 			NullString		`json:"thumbnail"`
+	SubredditID			NullString		`json:"subreddit_id"`
+	//Edited 				bool		`json:"edited"`
+	LinkFlairCSSClass 	NullString		`json:"link_flair_css_class"`
+	AuthorFlairCSSClass NullString		`json:"author_flair_css_class"`
+	Downs 				NullFloat		`json:"downs"`
+	//Saved 				bool		`json:"saved"`
+	//IsSelf 				bool		`json:"is_self"`
+	Permalink 			NullString		`json:"permalink"`
+	Name 				NullString		`json:"name"`
+	Created 			NullFloat		`json:"created"`
+	Url 				NullString		`json:"url"`
+	AuthorFlairText 	NullString		`json:"author_flair_text"`
+	Author 				NullString		`json:"author"`
+	CreatedUTC 			NullFloat		`json:"created_utc"`
+	//Media 				NullString		`json:"media"`
+	NumReports 			NullFloat		`json:"num_reports"`
+	Ups 				NullFloat		`json:"ups"`
+}
+
+type Entry struct {
+	Kind 				NullString		`json:"kind"`	
+	Data 				EData			`json:"data"`
+}
+
+type TData struct {
+	Modhash 			NullString		`json:"modhash"`
+	Children 			[]Entry			`json:"children"`
+	After 				NullString		`json:"after"`
+	Before 				NullString 		`json:"before"`
+}
+
+type Top struct {
+	Kind 				NullString		`json:"kind"`
+	Data 				TData			`json:"data"`
+	
+}
+
+func (t Top) String() string {
+	var ret []string
+	for _,child := range t.Data.Children {
+		ret = append(ret, string(child.Data.Title))
+	}
+	return strings.Join(ret, "\n")
+}
+
+// return an array of strings that can be easily iterated through 
+// and sent to irc.SendRaw
+func (t Top) List() []string {
+	var ret []string
+	for _, child := range t.Data.Children {
+		ret = append(ret, string(child.Data.Title) + " " + string(child.Data.Url))
+	}
+	return ret
+}
+
+func CheckError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
