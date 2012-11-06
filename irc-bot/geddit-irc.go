3ca092ada2a3e9df6e5923f3b02b1630ee49203c
Mike G <keep.it.gully@gmail.com>
2012-11-05 23:26:30 -0500

    Fixed index out of range on e.Message[8:] for @reddit commands with no args.  Added flow control in the form of a bool channel queue. Have to take turns. Maybe lower the pause between each send from 1 second to compensate.

--- irc-bot/geddit-irc.go
+++ irc-bot/geddit-irc.go
@@ -16,10 +16,14 @@ import (
 	irc "github.com/thoj/Go-IRC-Client-Library"
 )
 
-func slowSendIRC(s string, icon irc.IRCConnection) {
-	second := time.Second
-	//	index := len(s)
-	time.Sleep(second)
+// send strings to IRC
+func sendIRC(s []string, i *irc.IRCConnection, e *irc.IRCEvent, ready chan bool) {
+	i.SendRaw("PRIVMSG " + e.Arguments[0] + " :\x031,9d-(^_^)z \x039,1 check your PMs for /r/" + e.Message[8:])
+	for _, str := range s {
+		i.SendRaw("PRIVMSG " + e.Nick + " :" + str)
+		time.Sleep(time.Second*1)
+	}
+	ready <- true
 }
 
 func main() {
@@ -27,23 +31,29 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: %s server:port channel bot-username", os.Args[0])
 		os.Exit(1)
 	}
-	
+
+	ready := make(chan bool, 1)	
+	ready <- true
 	// http client
 	client := &http.Client{}
-
+	
 	// setup IRC
 	icon := irc.IRC(os.Args[3], "hahawtflol")
 	err := icon.Connect(os.Args[1])
 	geddit.CheckError(err)
+	
 	icon.AddCallback("001", func(e *irc.IRCEvent) { icon.Join("#" + os.Args[2]) })
 	//icon.AddCallback("JOIN", func(e *irc.IRCEvent) { 
 		//icon.SendRaw("PRIVMSG #" + os.Args[2] + " :SCV ready s(^_^)-b")
 	//})
+
 	icon.AddCallback("PRIVMSG", func(e *irc.IRCEvent) {
 		if strings.HasPrefix(e.Message, "@reddit") {
+			// check for message longer than '@reddit sub'
+			if len(e.Message) < 8 { return }
 			req, err := http.NewRequest("GET", "http://reddit.com/r/" + e.Message[8:] + ".json", nil)
 			req.Header.Set("User-Agent", "wtf_is_up ~ playing with Go-lang")
-	
+
 			// make request	
 			resp, err := client.Do(req)	
 			geddit.CheckError(err)
@@ -51,12 +61,12 @@ func main() {
 				log.Println("bad HTTP response: " + resp.Status)
 				return
 			}
-	
+
 			// read response
 			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(resp.Body)
 			geddit.CheckError(err)
-			
+		
 			// hackish way of dealing with XML returned from reddit redirect caused 
 			// by subreddit not existing	
 			if strings.HasPrefix(string(body), "<") {
@@ -68,13 +78,13 @@ func main() {
 			var jRep geddit.Top
 			err = json.Unmarshal(body, &jRep)
 			geddit.CheckError(err)
-
-			icon.SendRaw("PRIVMSG " + e.Arguments[0] + " :\x031,9d-(^_^)z \x039,1check your PMs for /r/" + e.Message[8:])
-			for _, str := range jRep.ToIRCStrings() {
-				icon.SendRaw("PRIVMSG " + e.Nick + " :" + str)
-				time.Sleep(time.Second*1)
-			}		
-		}
+			select {		
+			case <-ready:			
+				go sendIRC(jRep.ToIRCStrings(), icon, e, ready)
+			default:
+				icon.SendRaw("PRIVMSG " + e.Arguments[0] + " :\x031,4q-(v_v)z \x034,1 wait ur turn (flood control, l0l ")
+			}	
+		}		
 	})
 	icon.Loop()
 }
